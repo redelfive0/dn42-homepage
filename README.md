7c817e93e13ec993c0434de3067c8e65190037c8
Red Earth <redearth@50335033.xyz>
2026-09-05 23:57:05 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Red Earth Network Homepage
+
+Decentralized experimental network community.
+
+## Links
+
+- Internet: https://dn42.50335033.xyz/
+- dn42: https://network.redearth.dn42/
---
+++ astro.config.mjs
@@ -0,0 +1,23 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+import playformCompress from '@playform/compress';
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://dn42.50335033.xyz',
+    integrations: [
+        starlight({
+            title: 'Red Earth Network',
+            social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/redelfive0/dn42-homepage' }],
+            sidebar: [
+                {
+                    label: 'Services',
+                    items: [{ autogenerate: { directory: 'services' } }]
+                },
+            ],
+		}),
+        playformCompress()
+    ],
+});
